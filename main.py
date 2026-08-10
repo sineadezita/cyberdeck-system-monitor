252cede08206b43c1a3f1ad6ddf729306dc819b8
Sinead <s.ezita@icloud.com>
2026-08-10 23:37:45 +0100

    Add navy dark theme CSS and static file serving

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from collector import get_metrics, check_alerts
 from database import init_db, save_metrics, get_history
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.on_event("startup")
 def startup():
